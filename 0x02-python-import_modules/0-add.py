7d23c66dbd9fdf93f6d7ac4fe21cc6aad4d5c5f7
Emmanuel0846 <olaoluwaidowu166@gmail.com>
2023-08-15 01:29:03 -0700

    addition

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from custom_addition import add_numbers
+
+num1 = 1
+num2 = 2
+
+result = add_numbers(num1, num2)
+print("{0} added to {1} gives us {2}".format(num1, num2, result))
+
